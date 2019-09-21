69f98117353c848006227a17be16db10b9ab6396
Deon Kloppers <gjkloppers@gmail.com>
2019-09-21 10:28:24 +0200

    chanded hello.go

--- hello.go
+++ hello.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-fmt.Printf("Hello World\n")
-	
+	fmt.Printf("Hello World\n")
 
-
-fmt.Printf("Ja Ja")
 	fmt.Printf("Ja Ja")
 	fmt.Printf("Ja Ja")
+	fmt.Printf("Ja Ja")
+	fmt.Printf("GIT")
 }
